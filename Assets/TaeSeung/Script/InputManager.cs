fa20bc1585c74313f868415aa09f8245b38623d8
fluetanol <mike415415@naver.com>
2023-07-11 17:53:34 +0900

    Grid System Housing 개발 4
    그리드 배치 시스템 제작 중

--- Assets/TaeSeung/Script/InputManager.cs
+++ Assets/TaeSeung/Script/InputManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
@@ -12,6 +14,21 @@ public class InputManager : MonoBehaviour
 
     [SerializeField]
     private LayerMask placementLayermask;
+
+    public event Action OnClicked, OnExit;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnClicked?.Invoke();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
+    }
+
+    public bool IsPointerOverUI()
+        => EventSystem.current.IsPointerOverGameObject();
+
 
     public Vector3 GetSelectedMapPosition()
     {
