b62222ba1f340cda267f7b368537de769cb2f47a
taehyeoon <tedxpem@naver.com>
2023-08-04 02:00:33 +0900

    Feat: 펫 스탯 UI 구현
    InteractUIManager에서 관리
    직접 추적할 stat객체를 inspector에서 연결해줘야 함

--- Assets/TaeHyeon/Scripts/InteractUIManager.cs
+++ Assets/TaeHyeon/Scripts/InteractUIManager.cs
@@ -3,13 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Logger = ZipsAR.Logger;
 
 public class InteractUIManager : MonoBehaviour
 {
     [SerializeField] private Button exitBtn;
+    [SerializeField] private Canvas overlayCanvas;
+    [SerializeField] private PetStatBase trackingStat;
 
+    [SerializeField] private Slider fullnessSlider;
+    [SerializeField] private Slider cleanlinessSlider;
+    [SerializeField] private Slider tirednessSlider;
+    
     private void Awake()
     {
         exitBtn.onClick.AddListener(GameManager.Instance.QuitApp);
+    }
+
+    private void Start()
+    {
+        overlayCanvas.worldCamera = GameManager.Instance.player.gameObject.GetComponent<Camera>();
+        overlayCanvas.planeDistance = 0.11f;
+    }
+
+    private void Update()
+    {
+        fullnessSlider.value = trackingStat.fullness / 100f;
+        cleanlinessSlider.value = trackingStat.cleanliness / 100f;
+        tirednessSlider.value = trackingStat.tiredness / 100f;
     }
 }
