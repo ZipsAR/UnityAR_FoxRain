acbfd6b6c606ede1ef6c1093dd09dd6ee226778d
jaewonjung6446 <jaewon6446@naver.com>
2023-08-04 18:37:41 +0900

    상점 최종
    ToDo
    -버튼 클릭 음 찾기

--- Assets/Jaewon2/Script/ItemMov.cs
+++ Assets/Jaewon2/Script/ItemMov.cs
@@ -8,6 +8,7 @@ public class ItemMov : MonoBehaviour
     [SerializeField] private int ItemNum;
     public bool isSelected;
     public GameObject childitem;
+    public GameObject childitem_Inven;
     public GameObject viewObj;
     public GameObject viewObj_Inven;
     public Queue<GameObject> childs;
@@ -39,17 +40,32 @@ public class ItemMov : MonoBehaviour
     }
     public void ViewObj()
     {
-        if (this.CompareTag("Store"))
+        Debug.Log(this.gameObject.name);
+        if (this.gameObject.name == "ItemButton(Clone)")
         {
-            Debug.Log("비교");
-            StoreManager.Instance.viewItem_store = GameObject.Instantiate(childitem, viewObj.transform);
-            StoreManager.Instance.viewItem_store.gameObject.GetComponent<Transform>().localScale = viewScale;
+            if (childitem.CompareTag("furniture"))
+            {
+                StoreManager.Instance.viewItem_store = GameObject.Instantiate(childitem, viewObj.transform);
+                StoreManager.Instance.viewItem_store.gameObject.GetComponent<Transform>().localScale = viewScale;
+            }
+            else
+            {
+                StoreManager.Instance.viewItem_store = GameObject.Instantiate(childitem, viewObj.transform);
+                StoreManager.Instance.viewItem_store.gameObject.GetComponent<Transform>().localScale = new Vector3(800,800,800);
+            }
         }
-        if (this.CompareTag("Inven"))
+        if (this.gameObject.name == "ItemButton_Inven(Clone)")
         {
-            Debug.Log("비교");
-            StoreManager.Instance.viewItem_Inven = GameObject.Instantiate(childitem, viewObj_Inven.transform);
-            StoreManager.Instance.viewItem_Inven.gameObject.GetComponent<Transform>().localScale = viewScale;
+            if (childitem.CompareTag("furniture"))
+            {
+                StoreManager.Instance.viewItem_Inven = GameObject.Instantiate(childitem, viewObj_Inven.transform);
+                StoreManager.Instance.viewItem_Inven.gameObject.GetComponent<Transform>().localScale = viewScale;
+            }
+            else
+            {
+                StoreManager.Instance.viewItem_Inven = GameObject.Instantiate(childitem, viewObj_Inven.transform);
+                StoreManager.Instance.viewItem_Inven.gameObject.GetComponent<Transform>().localScale = new Vector3(800, 800, 800);
+            }
         }
     }
     //부모의 오브젝트의 레이어가 인벤토리 일 경우를 나눔
--- Assets/Jaewon2/Script/PanelManager.cs
+++ Assets/Jaewon2/Script/PanelManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour
 {
     public GameObject[] Panels;
+    public Text tagName;
     public GameObject currentPanel;
     public int panelNum;
     public void Start()
@@ -16,6 +18,7 @@ public class PanelManager : MonoBehaviour
             Panels[i].SetActive(false);
         }
         currentPanel.SetActive(true);
+        tagName.text = "Store-Furniture";
     }
     public void PanelCon_Right()
     {
@@ -30,6 +33,18 @@ public class PanelManager : MonoBehaviour
         {
             panelNum = -1;
         }
+        switch (panelNum)
+        {
+            case 0:
+                tagName.text = "Store-Furniture";
+                break;
+            case 1:
+                tagName.text = "Store-Toy";
+                break;
+            case 2:
+                tagName.text = "Store-Food";
+                break;
 
+        }
     }
 }
