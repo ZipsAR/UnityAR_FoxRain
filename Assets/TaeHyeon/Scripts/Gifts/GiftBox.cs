0935a63c56daf36fa2224bd6cfd5d72b05993988
taehyeoon <tedxpem@naver.com>
2023-08-07 05:06:27 +0900

    Feat: 박스를 열면 gift가 서서히 회전하면서 y축 위 방향으로 상승
    기프트 박스 열리는 애니메이션 시작 딜레이 부분 삭제

--- Assets/TaeHyeon/Scripts/Gifts/GiftBox.cs
+++ Assets/TaeHyeon/Scripts/Gifts/GiftBox.cs
@@ -6,19 +6,31 @@ using Logger = ZipsAR.Logger;
 
 public class GiftBox : MonoBehaviour
 {
+    private bool isOpened;
     private GameObject gift;
     [SerializeField] private Transform giftTransform;
-    private bool isOpened;
     [SerializeField] private Animator lidAnimator;
+
+    // Effects
     private GameObject curEffect;
-    
     [SerializeField] private GameObject idleEffect;
     [SerializeField] private GameObject openEffect;
     [SerializeField] private GameObject afterEffect;
+    
     private static readonly int Open = Animator.StringToHash("Open");
+
+    private float roatationTime;
+    private float rotationSpeed; // Rotation angle per second
+    private float risingTime;
+    private float risingDistance;
 
     private void Start()
     {
+        roatationTime = 4f;
+        rotationSpeed = 180f;
+        risingTime = 2f;
+        risingDistance = 0.05f;
+
         curEffect = Instantiate(idleEffect, transform);
     }
 
@@ -35,6 +47,9 @@ public class GiftBox : MonoBehaviour
             curEffect = Instantiate(openEffect, transform);
             lidAnimator.SetTrigger(Open);
             GetComponent<Collider>().enabled = false;
+            
+            StartCoroutine(StartRotation());
+            StartCoroutine(StartRising());
         }    
     }
 
@@ -45,4 +60,30 @@ public class GiftBox : MonoBehaviour
         if (gift.TryGetComponent(out Rigidbody component)) component.isKinematic = true;
     }
 
+    private IEnumerator StartRotation()
+    {
+        while (roatationTime > 0)
+        {
+            gift.transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+            roatationTime -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator StartRising()
+    {
+        float elapsedTime = 0f;
+        Vector3 initialPosition = gift.transform.position;
+        Vector3 targetPosition = initialPosition + Vector3.up * risingDistance;
+        
+        while (elapsedTime < risingTime)
+        {
+            float t = elapsedTime / risingTime;
+            gift.transform.position = Vector3.Lerp(initialPosition, targetPosition, t);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+    }
 }
