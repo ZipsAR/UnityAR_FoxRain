2baa68d051f37e2555adec8e5448c4a8e1cea07a
jaewonjung6446 <jaewon6446@naver.com>
2023-08-10 00:41:46 +0900

    UI배치

--- Assets/Jaewon2/Script/ItemButtonGen.cs
+++ Assets/Jaewon2/Script/ItemButtonGen.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemButtonGen : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class ItemButtonGen : MonoBehaviour
     public GameObject buttonPanel_Inven;
     public GameObject[] parentPanel = new GameObject[3];
     public GameObject[] parentPanel_Inven = new GameObject[3];
+    public Sprite buttonUI;
     private void Start()
     {
         ButtonGened = new GameObject[itemDBObjs.Length];
