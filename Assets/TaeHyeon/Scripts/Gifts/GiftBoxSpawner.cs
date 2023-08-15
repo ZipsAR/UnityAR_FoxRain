c01ffa37e6284e519217831ad74a2d5fad98cadb
taehyeoon <tedxpem@naver.com>
2023-08-15 14:08:47 +0900

    Fix: 레벨업 상자가 펫과 겹쳐 생성되던 오류 수정
    레벨업 상자 생성기준
    플레이어가 펫을 바라보았을 떄 기준으로 플레이어 앞 살짝 오른쪽에 생성됨

--- Assets/TaeHyeon/Scripts/Gifts/GiftBoxSpawner.cs
+++ Assets/TaeHyeon/Scripts/Gifts/GiftBoxSpawner.cs
@@ -29,25 +29,22 @@ public class GiftBoxSpawner : MonoBehaviour
         curPetPos = new Vector3(curPetPos.x, GameData.floorHeight, curPetPos.z);
         playerPos = new Vector3(playerPos.x, GameData.floorHeight, playerPos.z);
         
-        Vector3 spawnPos = GetPointBeforeDistance(curPetPos, playerPos, GameData.playerFrontDistance);
-
+        // Box Position
+        Vector3 playerToPetRight = (Quaternion.AngleAxis(90f,Vector3.up) * (curPetPos - playerPos)).normalized;
+        Vector3 spawnPos = Utils.GetPointBeforeDistance(curPetPos, playerPos, GameData.playerFrontDistance);
+        spawnPos += playerToPetRight * GameData.playerFrontDistance;
+        
+        // Box Rotation
         Quaternion spawnRotation = Quaternion.LookRotation(-(playerPos - spawnPos));
+        
+        // Instantiate
         GameObject spawnedObj = Instantiate(giftBoxObj, spawnPos, spawnRotation);
+        
         GiftBox giftBox = spawnedObj.GetComponent<GiftBox>();
         giftBox.SetCoinEarnedValue(coinEarnedValue);
         
+        // Set content of gift
         giftBox.SpawnGift(GetCoinObj());
-    }
-    
-    
-    private Vector3 GetPointBeforeDistance(Vector3 startPoint, Vector3 endPoint, float beforeDistance)
-    {
-        float distance = Vector3.Distance(startPoint, endPoint);
-        float x = endPoint.x - (beforeDistance / distance) * (endPoint.x - startPoint.x);
-        float y = startPoint.y;
-        float z = endPoint.z - (beforeDistance / distance) * (endPoint.z - startPoint.z);
-
-        return new Vector3(x, y, z);
     }
 
     private GameObject GetRandomItem()
