8196a344f2657ab625740a754a39c7439527462c
taehyeoon <tedxpem@naver.com>
2023-08-29 00:27:35 +0900

    Feat: 펫의 위치를 추적하는 화살표 구현
    펫이 시야에서 사라지는 경우, 화살표로 펫의 위치를 나타냄

---
+++ Assets/TaeHyeon/Scripts/Pets/PetArrowController.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Logger = ZipsAR.Logger;
+
+public class PetArrowController : MonoBehaviour
+{
+    private bool isPetInitialized;
+    private float angleDegrees;
+    private Camera mainCamera;
+    [SerializeField] private Canvas uiCanvas;
+
+    // Pet
+    private Transform petTransform;
+    private Vector3 petScreenPos;
+
+    // Arrow
+    [SerializeField] private GameObject arrowPrefab;
+    private GameObject arrowInstance;
+    private RectTransform arrowRectTransform;
+    private float circleBoundaryRadius;
+
+    // Screen
+    private int screenWidthHalf;
+    private int screenHeightHalf;
+    
+    private void OnEnable()
+    {
+        InteractEventManager.OnPetInitializedToAll -= OnPetInitializedToAll;
+        InteractEventManager.OnPetInitializedToAll += OnPetInitializedToAll;
+    }
+
+    private void OnDisable()
+    {
+        InteractEventManager.OnPetInitializedToAll -= OnPetInitializedToAll;
+    }
+
+    private void OnPetInitializedToAll(object sender, PetArgs e)
+    {
+        mainCamera = Camera.main;
+        
+        screenWidthHalf = Screen.width / 2;
+        screenHeightHalf = Screen.height / 2;
+
+        petTransform = e.petObj.transform;
+        circleBoundaryRadius = 300f;
+        isPetInitialized = true;
+    }
+
+    private void Update()
+    {
+        if(!isPetInitialized) return;
+
+        petScreenPos = GetPetScreenPos();
+
+        if (IsPetInView())
+        {
+            if (arrowInstance != null)
+            {
+                Destroy(arrowInstance);
+                arrowInstance = null;
+            }
+            return;
+        }
+        
+        if (arrowInstance == null)
+        {
+            arrowInstance = Instantiate(arrowPrefab, uiCanvas.transform);
+            arrowRectTransform = arrowInstance.GetComponent<RectTransform>();
+        }
+
+        // Set arrow Rotation
+        Vector2 cameraToPet = new Vector2(petScreenPos.x, petScreenPos.y).normalized;
+        angleDegrees = Mathf.Atan2(cameraToPet.y, cameraToPet.x) * Mathf.Rad2Deg;
+        arrowRectTransform.localRotation = Quaternion.Euler(0,0,angleDegrees);
+
+        // Set arrow Position
+        arrowRectTransform.localPosition = cameraToPet * circleBoundaryRadius;
+    }
+
+    private bool IsPetInView()
+    {
+        if (petScreenPos.x < -screenWidthHalf || petScreenPos.x > screenWidthHalf ||
+            petScreenPos.y < -screenHeightHalf || petScreenPos.y > screenHeightHalf ||
+            petScreenPos.z < 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private Vector3 GetPetScreenPos()
+    {
+        Vector3 petScreenPosLeftBottomAnchor = mainCamera.WorldToScreenPoint(petTransform.position);
+        Vector3 result;
+        result = new Vector3(
+            petScreenPosLeftBottomAnchor.x - screenWidthHalf, 
+            petScreenPosLeftBottomAnchor.y - screenHeightHalf, 
+            petScreenPosLeftBottomAnchor.z);
+
+        if (result.z < 0)
+        {
+            result.x *= -1;
+            result.y *= -1;
+        }
+        
+        return result;
+        
+    }
+}
